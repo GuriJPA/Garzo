2fad0411c50903c4a37dabc61166ecb4799dde54
Carlos Oller <carlos.oller@mi.unc.edu.ar>
2021-05-29 22:23:44 -0300

    base de datos actualizada

--- BD/garcon.sql
+++ BD/garcon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2021 a las 01:15:28
+-- Tiempo de generación: 30-05-2021 a las 01:22:39
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.6
 
