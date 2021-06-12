2326eca1b7af7c1476967fbabd8361d0fe67a70b
Fabri <megafagol@gmail.com>
2021-06-12 17:58:42 -0300

    Primer codigo para la funcion de instalacion

--- libs/app.php
+++ libs/app.php
@@ -6,83 +6,87 @@ class App{
     function __construct(){
         //echo "<p>Nueva app</p>";
 
-        $url = isset($_GET['url']) ? $_GET['url']: null;
-        $url = rtrim($url, '/');
-        $url = explode('/', $url);
+        if(file_exists("controllers/install.php")){
 
-        //var_dump($url);
+        }else{
 
-        //echo empty($url[0]);
+            $url = isset($_GET['url']) ? $_GET['url']: null;
+            $url = rtrim($url, '/');
+            $url = explode('/', $url);
 
-        session_start();
-    
+            //var_dump($url);
+
+            //echo empty($url[0]);
+
+            session_start();
         
-        if(!(isset($_SESSION['id_matriz']))){
-            $_SESSION['id_matriz'] = 0;
-        }
-
-        if(!(isset($_SESSION['mesa']))){
-            $_SESSION['mesa'] = 1;
-        }
-
-        if(!(isset($_SESSION['matriz_pedidos']))){
-            $_SESSION['matriz_pedidos'] = array();
-        }
-        
-        // cuando se ingresa sin definir controlador
-        if(empty($url[0])){
-            $archivoController = 'controllers/main.php';
-            require_once $archivoController;
-            $controller = new Main();
-            $controller->loadModel('main');
-            $controller->render();
-            return false;
-        }
-
-        $url_controller = $url[0];
-        
-        
-        if(!(isset($_SESSION['logueado']))){
-            $_SESSION['logueado'] = FALSE;
-        }
-
-        if($url_controller == 'admin' && $_SESSION['logueado'] == FALSE){
-            $url_controller = 'login';
-            $url= null;
-            $url[0] = '';
-        }
-
-        $archivoController = 'controllers/' . $url_controller . '.php';
-
-        if(file_exists($archivoController)){
-            require_once $archivoController;
-
-            // inicializar controlador
-            $controller = new $url_controller;
-            $controller->loadModel($url_controller);
             
-            // # elementos del arreglo
-            $nparam = sizeof($url);
+            if(!(isset($_SESSION['id_matriz']))){
+                $_SESSION['id_matriz'] = 0;
+            }
 
-            if($nparam > 1){
-                if($nparam > 2){
-                    $param = [];
-                    for($i = 2; $i<$nparam; $i++){
-                        array_push($param, $url[$i]);
+            if(!(isset($_SESSION['mesa']))){
+                $_SESSION['mesa'] = 1;
+            }
+
+            if(!(isset($_SESSION['matriz_pedidos']))){
+                $_SESSION['matriz_pedidos'] = array();
+            }
+            
+            // cuando se ingresa sin definir controlador
+            if(empty($url[0])){
+                $archivoController = 'controllers/main.php';
+                require_once $archivoController;
+                $controller = new Main();
+                $controller->loadModel('main');
+                $controller->render();
+                return false;
+            }
+
+            $url_controller = $url[0];
+            
+            
+            if(!(isset($_SESSION['logueado']))){
+                $_SESSION['logueado'] = FALSE;
+            }
+
+            if($url_controller == 'admin' && $_SESSION['logueado'] == FALSE){
+                $url_controller = 'login';
+                $url= null;
+                $url[0] = '';
+            }
+
+            $archivoController = 'controllers/' . $url_controller . '.php';
+
+            if(file_exists($archivoController)){
+                require_once $archivoController;
+
+                // inicializar controlador
+                $controller = new $url_controller;
+                $controller->loadModel($url_controller);
+                
+                // # elementos del arreglo
+                $nparam = sizeof($url);
+
+                if($nparam > 1){
+                    if($nparam > 2){
+                        $param = [];
+                        for($i = 2; $i<$nparam; $i++){
+                            array_push($param, $url[$i]);
+                        }
+                        $controller->{$url[1]}($param);
+                    }else{
+                        $controller->{$url[1]}();
                     }
-                    $controller->{$url[1]}($param);
                 }else{
-                    $controller->{$url[1]}();
+                    $controller->render();
                 }
             }else{
-                $controller->render();
+                $controller = new Errores();
             }
-        }else{
-            $controller = new Errores();
-        }
             
         
-        
+        }
         
         
     }
