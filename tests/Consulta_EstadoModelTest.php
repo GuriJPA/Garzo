782255bb8e78232f625e471fb791e15944e34c9d
Guri <gjpa2011@hotmail.com>
2021-06-12 21:15:13 -0300

    se termino el test_set_estado_finalizado

--- tests/Consulta_EstadoModelTest.php
+++ tests/Consulta_EstadoModelTest.php
@@ -19,28 +19,28 @@ class Consulta_EstadoModelTest extends TestCase
         $items = [];
             
                 $item = new Estado();
-                $item->$id_estado = 2;
-                $item->$nombre = 'ASD';
+                $item->id_estado = 1;
+                $item->nombre = 'Pendiente a Tomar';
                 array_push($items, $item);
 
                 $item1 = new Estado();
-                $item1->$id_estado = 2;
-                $item1->$nombre = 'En preparacion';
+                $item1->id_estado = 2;
+                $item1->nombre = 'En preparacion';
                 array_push($items, $item1);
 
                 $item2 = new Estado();
-                $item2->$id_estado = 3;
-                $item2->$nombre = 'Listo';
+                $item2->id_estado = 3;
+                $item2->nombre = 'Listo';
                 array_push($items, $item2);
 
                 $item3 = new Estado();
-                $item3->$id_estado = 4;
-                $item3->$nombre = 'Pidio Cuenta';
+                $item3->id_estado = 4;
+                $item3->nombre = 'Pidio Cuenta';
                 array_push($items, $item3);
 
                 $item4 = new Estado();
-                $item4->$id_estado = 5;
-                $item4->$nombre = 'Finalizado';
+                $item4->id_estado = 5;
+                $item4->nombre = 'Finalizado';
                 array_push($items, $item4);
 
                 $this->assertEquals($items, $this->cp->get_estados());
